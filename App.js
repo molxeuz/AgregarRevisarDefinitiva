8681edb2a933333372c2986c63fa8cea1231ebcf
Mateo Molina Gonzalez <89753117+molxeuz@users.noreply.github.com>
2023-09-28 17:17:18 -0500

    Update App.js

--- App.js
+++ App.js
@@ -7,24 +7,17 @@ import Form from './components/Form';
 import AdvanceSearch from './components/AdvanceSearch';
 
 const Stack = createStackNavigator();
-
 const App = () => {
-
   return (
-
     <View style={ styles.app }>
       <NavigationContainer>
-
         <Stack.Navigator initialRouteName="Form">
           <Stack.Screen name="Form" component={Form} />
           <Stack.Screen name="AdvanceSearch" component={AdvanceSearch} />
         </Stack.Navigator>
-
       </NavigationContainer>
     </View>
-
   );
-
 };
 
 export default App;
